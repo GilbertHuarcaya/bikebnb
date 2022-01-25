1f6fe5bc6f26f1a2b604b886aa7bf799859c1734
Jesus Castilla <jesus.jcg8@gmail.com>
2022-01-25 17:38:40 -0500

    rentals controller, added def & edit action

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,14 @@
 class RentalsController < ApplicationController
+
+  def edit
+    @bike = Bike.find(params[:bike_id])
+
+  end
+
+  def update
+    @bike = Bike.find(params[:bike_id])
+    @bike.available = false
+    @bike.update
+    redirect_to bike_path(@bike)
+  end
 end
