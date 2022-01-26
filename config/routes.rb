8c179c58ef1d155b4e63485f74cdbe5d76708f04
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-01-26 16:53:42 -0500

    myrentals complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,12 @@ Rails.application.routes.draw do
       get :top
       get :my_bikes
     end
-    resources :rentals, except: [:destroy, :index] do
-      collection do
-        get :bike_rentals
-        get :my_rentals
-      end
+    resources :rentals, except: [:destroy, :index]
+  end
+  resources :rentals, only: [:destroy] do
+    collection do
+      get :my_rentals
     end
   end
-  resources :rental, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
