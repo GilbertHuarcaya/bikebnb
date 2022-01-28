7941a9b98acd214e1bd6901badce5a990d65a14c
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-01-28 14:11:10 -0500

    completado 2

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,8 +2,6 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.only-home');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      console.log(window.scrollY);
-      console.log(window.innerHeight);
       if (window.scrollY >= 76 && window.scrollY <= 550) {
         navbar.classList.add('fixed-only-home');
       } else {
